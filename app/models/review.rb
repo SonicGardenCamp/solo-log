7d8a8acd9c7809e75c72895fe82d14d81a477641
Akito <sza9ki8to7@gmail.com>
2023-08-29 04:50:24 +0000

    add validates review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :shop
+  
+  validates :rate, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
+  validates :comment, length: { maximum: 140 }, allow_nil: true
+  validates :exist_counter_sheets?, inclusion: { in: [true, false] }
+  validates :frequent_solo_visitors, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
+  validates :exist_solo_tables?, inclusion: { in: [true, false] }
+  validates :easy_to_order, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
+  validates :delivery_speed, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
+  validates :calmness, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
 end
