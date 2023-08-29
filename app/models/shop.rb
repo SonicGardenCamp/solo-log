019b650efcd90ac4ae266db4ff5473eee960e872
Akito <sza9ki8to7@gmail.com>
2023-08-29 07:03:15 +0000

    make shop model validation

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,10 @@
 class Shop < ApplicationRecord
   has_many :reviews
+  
+  validates :name, presence: true, length: { maximum: 60 }
+  validates :address, presence: true, length: { maximum: 200 }
+  validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+  #後で変更する
+  validates :genre, length: { maximum: 50 }
 end
