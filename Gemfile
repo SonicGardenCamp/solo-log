a499246e5ca90daccd0a87601ffa84a5c0a162b3
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2023-08-30 15:33:16 +0900

    railsアプリケーションの日本語化、reviewコントローラのcreateアクション作成、dbの制約を変更

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem "rails",           "7.0.4"
-# gem "bootstrap-sass",  "3.4.1"
 gem 'bootstrap', '~> 5.0.2'
 gem 'jquery-rails'
 gem "faker"
@@ -16,9 +15,10 @@ gem "puma",            "5.6.4"
 gem "bootsnap",        "1.12.0", require: false
 gem "devise"
 gem 'carrierwave'
+gem 'rails-i18n', '~> 7.0.0'
+gem "pg", "1.3.5"
 
 group :development, :test do
-  gem "sqlite3", "1.4.2"
   gem "debug",   "1.5.0", platforms: %i[ mri mingw x64_mingw ]
 end
 
@@ -35,10 +35,6 @@ group :test do
   gem "minitest-reporters",       "1.5.0"
   gem "guard",                    "2.18.0"
   gem "guard-minitest",           "2.4.6"
-end
-
-group :production do
-  gem "pg", "1.3.5"
 end
 
 # Windows ではタイムゾーン情報用の tzinfo-data gem を含める必要があります
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,12 @@
 class Review < ApplicationRecord
-  belongs_to :shop
+  belongs_to :shop, optional: true
   mount_uploader :image, ImageUploader
-  validates :rate, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rate, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
   validates :comment, length: { maximum: 140 }
-  validates :exist_counter_sheets?, inclusion: { in: [true, false] }
-  validates :frequent_solo_visitors, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  validates :exist_solo_tables?, inclusion: { in: [true, false] }
-  validates :easy_to_order, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  validates :delivery_speed, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  validates :calmness, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :exist_counter_sheets?, inclusion: { in: [true, false] }, allow_blank: true
+  validates :frequent_solo_visitors, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, allow_blank: true }
+  validates :exist_solo_tables?, inclusion: { in: [true, false] }, allow_blank: true
+  validates :easy_to_order, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, allow_blank: true }
+  validates :delivery_speed, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, allow_blank: true }
+  validates :calmness, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, allow_blank: true }
 end
