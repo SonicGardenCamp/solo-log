0b95398acabffaf64601b8b4f5ba89254644d1b9
54chair <takukoshi3721@gmail.com>
2023-08-30 04:24:53 +0000

    feat: モデルにマウント

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :shop
-  
+  mount_uploader :image, ImageUploader
   validates :rate, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   validates :comment, length: { maximum: 140 }
   validates :exist_counter_sheets?, inclusion: { in: [true, false] }
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,6 @@
 class Shop < ApplicationRecord
   has_many :reviews
-  
+  mount_uploader :image, ImageUploader
   validates :name, presence: true, length: { maximum: 60 }
   validates :address, presence: true, length: { maximum: 200 }
   validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
