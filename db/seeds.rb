45b4ca8e428786cbd7df93bc3b81ef51bd20fb2b
54chair <takukoshi3721@gmail.com>
2023-09-04 06:22:26 +0000

    seedファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,28 +8,31 @@ def give_review
   rand(1..5)
 end
 
-users = []
 10.times do
-  users << User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password(min_length: 8),
-    confirmed_at: Time.now
-  )
-end
-10.times do
-  users << User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password(min_length: 8)
-  )
-end
-
-20.times do
   shop = Shop.new(latitude: Faker::Address.latitude.ceil(6),
                   longitude: Faker::Address.longitude.ceil(6),
                   address: Faker::Address.full_address,
                   name: Faker::Lorem.sentence,
                   genre: Faker::Food.ethnic_category)
   shop.save!
+end
+
+
+20.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
+  )
+  user.save!
+  user.reviews.create!( rate: rand(1..5),
+                      comment: Faker::Lorem.paragraph(sentence_count: 4),
+                      counter_sheets_available: random_true_false,
+                      frequent_solo_visitors: give_review,
+                      solo_tables_available: random_true_false,
+                      easy_to_order: give_review,
+                      delivery_speed: give_review,
+                      calmness: give_review,
+                      shop_id: rand(1..Shop.count))
 end
 
 Shop.all.each do |shop|
