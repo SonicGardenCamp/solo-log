38404b69d146b2347699d87cad683a3e72afe917
54chair <takukoshi3721@gmail.com>
2023-09-08 01:19:26 +0000

    店舗の編集と削除を実装

--- app/controllers/admins/shops_controller.rb
+++ app/controllers/admins/shops_controller.rb
@@ -17,6 +17,35 @@ class Admins::ShopsController < ApplicationController
     end
   end
   
+  def edit
+    @shop = Shop.find(params[:id])
+  end
+  
+  def update
+    @shop = Shop.find(params[:id])
+    if @shop.update(shop_params)
+      @shop.update_lat_and_long
+      flash[:notice] = "店舗の編集に成功しました。"
+      redirect_to root_path
+    else
+      flash[:alert] = "店舗の編集に失敗しました。"
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @shop = Shop.find(params[:id])
+    if @shop.destroy
+      flash[:notice] = "店舗の削除に成功しました。"
+      redirect_to root_path
+    else
+      flash[:alert] = "店舗の削除に失敗しました。"
+      redirect_to root_path
+    end
+  end
+  
+  private
+  
   def shop_params
     params.require(:shop).permit(:name, :image, :address, :genre)
   end
